3155d90d5301bdb597a61ef2e7f3d30a20f8442d
Joel Ninan Johnson <joel.cs19@bmsce.ac.in>
2021-12-02 09:07:18 +0530

    Add files via upload

---
+++ LAB6/gross_salary.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+echo "Enter the basic salary: "
+read basic
+
+da=$(echo "0.1 * $basic" | bc)
+hra=$(echo "0.2 * $basic" | bc)
+
+gross=$(echo "$basic + $da + $hra" | bc)
+echo "Gross salary of the employee: $gross"
+
